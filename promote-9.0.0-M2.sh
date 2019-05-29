126423eed0c3b8a2a67e1c266930a2ed13733197
Andrea Del Bene <adelbene@apache.org>
2019-05-29 19:35:51 +0200

    Added post-release scripts and vote/release email templates

---
+++ promote-9.0.0-M2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "Promoting release 9.0.0-M2
+
+Actions about to be performed:
+------------------------------
+
+$(cat $0 | tail -n +14)
+
+------------------------------------------"
+
+read -p "Press enter to continue or CTRL-C to abort"
+
+# push the release tag to ASF git repo
+
+git push origin rel/wicket-9.0.0-M2
+
+# promote the source distribution by moving it from the staging area to the release area
+
+svn mv https://dist.apache.org/repos/dist/dev/wicket/9.0.0-M2 https://dist.apache.org/repos/dist/release/wicket -m "Upload release to the mirrors"
+
+mvn org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-release -DstagingRepositoryId= -DnexusUrl=https://repository.apache.org -DserverId=apache.releases.https -Ddescription="Release vote has passed"
+
+# Renumber the next development iteration 9.0.0-M3-SNAPSHOT:
+
+git checkout master
+mvn release:update-versions --batch-mode
+mvn versions:set versions:commit -DnewVersion=9.0.0-M3-SNAPSHOT
+git add --all
+
+echo "
+Check the new versions and commit and push them to origin:
+
+  git commit -m "Start next development version"
+  git push
+
+Remove the previous version of Wicket using this command:
+
+  svn rm https://dist.apache.org/repos/dist/release/wicket/9.0.0-M2-SNAPSHOT -m \"Remove previous version from mirrors\"
+
+"
